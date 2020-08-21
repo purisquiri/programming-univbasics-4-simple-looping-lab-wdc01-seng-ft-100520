ee54f5140c3aba8f1a6425455ad3a1cbe5922821
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-21 14:48:14 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,9 @@
 
 # Write your methods here
-def loop_message_five_times(string)
-  message = string * 5
-  puts message
+def loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
 end
